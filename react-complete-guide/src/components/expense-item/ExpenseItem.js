dbd728a0cc9ad4962184e490cd606c66db166aab
ODOBRAN <Good.Morning.Vietnam.1991>
2022-06-18 13:28:29 +0300

    Cleaned the ExpenseItem component

--- react-complete-guide/src/components/expense-item/ExpenseItem.js
+++ react-complete-guide/src/components/expense-item/ExpenseItem.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Card from "../card/Card";
 import ExpenseDate from "../expense-date/ExpenseDate";
 import "./ExpenseItem.css";
@@ -7,13 +6,11 @@ function ExpenseItem(props) {
   const expenseDate = props.date;
   const expenseAmount = props.amount;
 
-  const [title, setTitle] = useState(props.title);
-
   return (
     <Card className="expense-item">
       <ExpenseDate expenseDate={expenseDate}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </Card>
