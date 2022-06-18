7b5e30ae159b6fcf7f1b796351c95cbb4b002173
ODOBRAN <Good.Morning.Vietnam.1991>
2022-06-18 14:00:54 +0300

    Dynamically generating ExpenseItem list, and filtering them by year

--- react-complete-guide/src/components/expenses/Expenses.js
+++ react-complete-guide/src/components/expenses/Expenses.js
@@ -10,7 +10,6 @@ function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
 
   function filterChangeHandler(selectedOption) {
-    console.log(selectedOption);
     setFilteredYear(selectedOption);
   }
 
@@ -20,26 +19,16 @@ function Expenses(props) {
         onFilterChanged={filterChangeHandler}
         selected={filteredYear}
       ></ExpensesFilter>
-      <ExpenseItem
-        title={items[0].title}
-        amount={items[0].amount}
-        date={items[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={items[1].title}
-        amount={items[1].amount}
-        date={items[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={items[2].title}
-        amount={items[2].amount}
-        date={items[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={items[3].title}
-        amount={items[3].amount}
-        date={items[3].date}
-      ></ExpenseItem>
+      {items
+        .filter((item) => +filteredYear === item.date.getFullYear())
+        .map((item) => (
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          ></ExpenseItem>
+        ))}
     </Card>
   );
 }
