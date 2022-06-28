386b2a9ad7e855c3d033d9ff564e6eb1112c9389
ODOBRAN <Good.Morning.Vietnam.1991>
2022-06-28 14:39:51 +0300

    Added bump class on cart button as a side effect

--- food-order-app/src/components/layout/header/cart-button/CartButton.js
+++ food-order-app/src/components/layout/header/cart-button/CartButton.js
@@ -1,17 +1,32 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../../../store/cart-context";
 import CartIcon from "../../../Cart/cart-icon/CartIcon";
 import classes from "./CartButton.module.css";
 
 function CartButton(props) {
+  const [btnIsHighlighted, setButtonIsHighlighted] = useState(false);
   const ctx = useContext(CartContext);
+  const { items } = ctx;
 
-  const numberOfCartItems = ctx.items.reduce((current, item) => {
+  const numberOfCartItems = items.reduce((current, item) => {
     return current + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  } `;
+
+  useEffect(() => {
+    if (numberOfCartItems === 0) {
+      return;
+    }
+    setButtonIsHighlighted(true);
+    const timer = setTimeout(() => setButtonIsHighlighted(false), 300);
+    return () => clearTimeout(timer);
+  }, [numberOfCartItems]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon></CartIcon>
       </span>
