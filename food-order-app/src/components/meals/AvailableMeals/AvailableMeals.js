d33bddc960c25032e2b2c0cc62ee128da334b28d
ODOBRAN <Good.Morning.Vietnam.1991>
2022-07-05 10:20:58 +0300

    Fetching meals from firebase

--- food-order-app/src/components/meals/AvailableMeals/AvailableMeals.js
+++ food-order-app/src/components/meals/AvailableMeals/AvailableMeals.js
@@ -1,38 +1,53 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styles from "./AvailableMeals.module.css";
+import { useEffect, useState } from "react";
 import Card from "../../layout/Card/Card";
 import MealItem from "../MealItem/MealItem";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import styles from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-a8b9e-default-rtdb.firebaseio.com/meals.son"
+      );
+      if (!response.ok) {
+        throw Error("Something went wrong!");
+      }
+      const resData = await response.json();
+      const loadedMeals = [];
+      for (let key in resData) {
+        loadedMeals.push({
+          id: key,
+          name: resData[key].name,
+          description: resData[key].description,
+          price: resData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+    };
+
+    try {
+      fetchMeals().catch((err) => {
+        setError(err.message);
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
