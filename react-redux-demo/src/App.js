f4af4bd939e3040fde93647c3c408898bfe8ef2f
ODOBRAN <Good.Morning.Vietnam.1991>
2022-07-05 17:07:37 +0300

    Added auth reducer

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -2,12 +2,15 @@ import { Fragment } from "react";
 import Counter from "./components/Counter";
 import Auth from "./components/Auth";
 import Header from "./components/Header";
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   return (
     <Fragment>
       <Header></Header>
-      <Auth></Auth>
+      {isAuthenticated ? <UserProfile></UserProfile> : <Auth></Auth>}
       <Counter />
     </Fragment>
   );
